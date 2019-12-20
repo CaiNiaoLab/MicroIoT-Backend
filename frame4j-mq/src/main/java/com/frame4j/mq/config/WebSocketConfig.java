1875ac7244d477d325921655fbcefd03f7190416
741026254@qq.com <Jyc741026254>
2019-12-20 14:37:12 +0800

    新增websocket相关配置

--- frame4j-mq/src/main/java/com/frame4j/mq/config/WebSocketConfig.java
+++ frame4j-mq/src/main/java/com/frame4j/mq/config/WebSocketConfig.java
@@ -2,17 +2,17 @@ package com.frame4j.mq.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
- * 开启WebSocket支持
- * @author zhengkai
+ * @author: mj
  */
 @Configuration
 public class WebSocketConfig {
 
-    /*@Bean
+    @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
-    }*/
+    }
 
 }
